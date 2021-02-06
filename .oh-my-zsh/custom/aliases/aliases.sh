bfac2df665328a6bccd1a51ee927808fd8394914
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2021-02-06 20:05:10 +0000

    Init 2021-02-06

---
+++ .oh-my-zsh/custom/aliases/aliases.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+alias polybar=/home/chaitanya/.config/polybar/launch.sh
+alias lector=/home/chaitanya/anaconda3/bin/lector
+alias adb=/home/chaitanya/bin/platform-tools/adb
+alias .=pwd
+alias xres="xrdb /home/chaitanya/.Xresources"
