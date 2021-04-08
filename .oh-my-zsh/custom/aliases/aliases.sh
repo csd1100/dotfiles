3dfc3db84344b6b02ebd8a3597d4f5ef9cfcee0b
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2021-04-08 22:49:36 +0000

    RefactorBKP

--- .oh-my-zsh/custom/aliases/aliases.sh
+++ .oh-my-zsh/custom/aliases/aliases.sh
@@ -1,6 +1,4 @@
 #!/bin/zsh
-alias polybar=/home/chaitanya/.config/polybar/launch.sh
-alias lector=/home/chaitanya/anaconda3/bin/lector
 alias adb=/home/chaitanya/bin/platform-tools/adb
 alias .=pwd
 alias xres="xrdb /home/chaitanya/.Xresources"
