454b4879c818474d588e78b8c1ad8cdf8c8f7a96
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2021-12-24 19:53:20 +0530

    Refactor Update

--- .oh-my-zsh/custom/aliases/aliases.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-alias adb=/home/chaitanya/bin/platform-tools/adb
-alias .=pwd
-alias xres="xrdb /home/chaitanya/.Xresources"
--- 2021-01/zsh/aliases/aliases.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-alias polybar=/home/chaitanya/.config/polybar/launch.sh
-alias lector=/home/chaitanya/anaconda3/bin/lector
-alias adb=/home/chaitanya/bin/platform-tools/adb
-alias .=pwd
