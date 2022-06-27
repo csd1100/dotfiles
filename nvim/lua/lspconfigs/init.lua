8269d787e6f0f1c74ec12d630e5a86bbb6d7cae6
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2022-06-27 23:19:20 +0530

    [m1](nvim){+} Fixed lua lsp

--- nvim/lua/lspconfigs/init.lua
+++ nvim/lua/lspconfigs/init.lua
@@ -1,32 +1,39 @@
 local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.keymap.set(mode, lhs, rhs, options)
+    local options = {
+        noremap = true,
+        silent = true
+    }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
 end
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  map('n', '<leader>ldc', vim.lsp.buf.declaration, bufopts)
-  map('n', '<leader>ld', vim.lsp.buf.definition, bufopts)
-  map('n', 'K', vim.lsp.buf.hover, bufopts)
-  map('n', '<leader>li', vim.lsp.buf.implementation, bufopts)
-  map('n', '<leader>lk', vim.lsp.buf.signature_help, bufopts)
-  map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  map('n', '<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  map('n', '<leader>ltd', vim.lsp.buf.type_definition, bufopts)
-  map('n', '<leader>lrn', vim.lsp.buf.rename, bufopts)
-  map('n', '<leader>lca', vim.lsp.buf.code_action, bufopts)
-  map('n', '<leader>lr', vim.lsp.buf.references, bufopts)
-  map('n', '<leader>lf', vim.lsp.buf.formatting, bufopts)
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = {
+        noremap = true,
+        silent = true,
+        buffer = bufnr
+    }
+    map('n', '<leader>ldc', vim.lsp.buf.declaration, bufopts)
+    map('n', '<leader>ld', vim.lsp.buf.definition, bufopts)
+    map('n', 'K', vim.lsp.buf.hover, bufopts)
+    map('n', '<leader>li', vim.lsp.buf.implementation, bufopts)
+    map('n', '<leader>lk', vim.lsp.buf.signature_help, bufopts)
+    map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    map('n', '<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    map('n', '<leader>ltd', vim.lsp.buf.type_definition, bufopts)
+    map('n', '<leader>lrn', vim.lsp.buf.rename, bufopts)
+    map('n', '<leader>lca', vim.lsp.buf.code_action, bufopts)
+    map('n', '<leader>lr', vim.lsp.buf.references, bufopts)
+    map('n', '<leader>lf', vim.lsp.buf.formatting, bufopts)
 end
 
 require("lspconfigs/nvim-lsp-installer-conf").setup()
@@ -36,16 +43,36 @@ local lspconfig = require("lspconfig")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-local servers = { 'bashls', 'emmet_ls', 'eslint' , 'rust_analyzer',
-    'grammarly', 'jsonls', 'kotlin_language_server', 'pyright', 'tsserver',
-    'marksman'
-}
+local servers = {'bashls', 'emmet_ls', 'eslint', 'rust_analyzer', 'grammarly', 'jsonls', 'kotlin_language_server',
+                 'pyright', 'tsserver', 'marksman', 'sumneko_lua'}
 
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         on_attach = on_attach,
-        capabilities = capabilities,
+        capabilities = capabilities
     }
 end
+
+-- sumneko_lua config --
+lspconfig['sumneko_lua']['settings'] = {
+    Lua = {
+        runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+        },
+        diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {'vim'}
+        },
+        workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true)
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+            enable = false
+        }
+    }
+}
 
 require('lspconfigs/snippets').setup()
