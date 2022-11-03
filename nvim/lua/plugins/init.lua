fed74db7a655ed175282a91961d34bb8d98956ac
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2022-11-03 11:45:15 +0530

    [main](nvim){=} Fixed new nvim setup

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,7 +1,6 @@
 local status, packer = pcall(require, 'packer')
 if not status then
-    vim.notify('failed to load packer', 'error')
-    return
+    vim.notify('packer not installed.', 'error')
 end
 
 local fn = vim.fn
@@ -10,12 +9,19 @@ local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if fn.isdirectory(install_path) == 0 then
-    fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.notify('installing packer...')
+    fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
     vim.cmd [[packadd packer.nvim]]
 
     local status, _ = pcall(require, 'plugins.plugin_specification')
     if not status then
         vim.notify('failed to load plugin specifications', 'error')
+        return
+    end
+
+    local status, packer = pcall(require, 'packer')
+    if not status then
+        vim.notify('failed to load packer', 'error')
         return
     end
 
