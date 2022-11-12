7904a5af13693c36030dc6c3bb6dbf8adbc538f2
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2022-11-12 17:36:20 +0530

    [main](nvim){+} Added neotest
    - Also formatted file with stylelua

--- nvim/lua/plugins/configs/test.lua
+++ nvim/lua/plugins/configs/test.lua
@@ -1,40 +1,46 @@
-local status, t = pcall(require, 'nvim-test')
+local status, neotest = pcall(require, "neotest")
 if not status then
-    vim.notify('failed to load nvim-test', 'error')
-    return
+	vim.notify("failed to load neotest", "error")
+	return
 end
 
 local M = {}
 
 function M.setup()
+	local conf = {
+		adapters = {
+			require("neotest-rust")({}),
+		},
+	}
 
-    local conf = {
-        run = true, -- run tests (using for debug)
-        commands_create = true, -- create commands (TestFile, TestLast, ...)
-        filename_modifier = ':.', -- modify filenames before tests run(:h filename-modifiers)
-        silent = false, -- less notifications
-        term = 'terminal', -- a terminal to run ('terminal'|'toggleterm')
-        termOpts = {
-            direction = 'vertical', -- terminal's direction ('horizontal'|'vertical'|'float')
-            width = 96, -- terminal's width (for vertical|float)
-            height = 24, -- terminal's height (for horizontal|float)
-            go_back = false, -- return focus to original window after executing
-            stopinsert = 'auto', -- exit from insert mode (true|false|'auto')
-            keep_one = true -- keep only one terminal for testing
-        },
-        runners = { -- setup tests runners
-            javascript = 'nvim-test.runners.mocha',
-            rust = 'nvim-test.runners.cargo-test'
-        }
-    }
+	neotest.setup(conf)
 
-    t.setup(conf)
+	local status, nvim_test = pcall(require, "nvim-test")
+	if not status then
+		vim.notify("failed to load nvim-test", "error")
+		return
+	end
 
-    local test_runners = {'mocha'}
-    for _, runner in ipairs(test_runners) do
-        require('plugins.configs.test-runner.' .. runner).setup()
-    end
-
+	local nvim_test_conf = {
+		run = true, -- run tests (using for debug)
+		commands_create = true, -- create commands (TestFile, TestLast, ...)
+		filename_modifier = ":.", -- modify filenames before tests run(:h filename-modifiers)
+		silent = false, -- less notifications
+		term = "terminal", -- a terminal to run ('terminal'|'toggleterm')
+		termOpts = {
+			direction = "vertical", -- terminal's direction ('horizontal'|'vertical'|'float')
+			width = 96, -- terminal's width (for vertical|float)
+			height = 24, -- terminal's height (for horizontal|float)
+			go_back = false, -- return focus to original window after executing
+			stopinsert = "auto", -- exit from insert mode (true|false|'auto')
+			keep_one = true, -- keep only one terminal for testing
+		},
+		runners = { -- setup tests runners
+			javascript = "nvim-test.runners.mocha",
+		},
+	}
+	nvim_test.setup(nvim_test_conf)
+	require("plugins.configs.test-runner.mocha").setup()
 end
 
 return M
