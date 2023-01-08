3d0d12213a5d88316c25aaec81f739631a7b11af
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2023-01-08 21:24:33 +0530

    (qtile) Removed entirely

--- qtile/.config/qtile/screens.py
+++
@@ -1,7 +0,0 @@
-from libqtile.config import Screen
-
-from bars import bottom_bar, top_bar
-
-main_screen = Screen(top=top_bar,
-                     bottom=bottom_bar)
-screens = [main_screen]
--- qtile/.config/qtile/vars.py
+++
@@ -1,5 +0,0 @@
-from pywal import wallpaper
-
-term = 'kitty'
-default_browser = 'google-chrome-stable'
-default_text_editor = 'code'
