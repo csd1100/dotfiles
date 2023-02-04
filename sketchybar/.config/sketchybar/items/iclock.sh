903ea4e66d1e349498719d700ef4453eda0db940
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2023-02-04 11:09:32 +0530

    (sketchybar) Fix clock on mouse enter display

--- sketchybar/.config/sketchybar/items/iclock.sh
+++ sketchybar/.config/sketchybar/items/iclock.sh
@@ -5,5 +5,5 @@ sketchybar --subscribe clock system_woke mouse.entered mouse.exited.global
 
 sketchybar --set clock \
 	label.drawing=on \
-	update_freq=1 \
+	update_freq=5 \
 	script="$PLUGIN_DIR/pclock.sh"
