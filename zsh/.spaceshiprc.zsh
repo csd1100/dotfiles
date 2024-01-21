01152a757b4acadbd303b96127d4290249b9a119
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2024-01-21 15:50:12 +0530

    (zsh) update spaceshiprc

--- zsh/.spaceshiprc.zsh
+++ zsh/.spaceshiprc.zsh
@@ -2,11 +2,9 @@ SPACESHIP_PROMPT_ORDER=(
     user          # Username section
     dir           # Current directory section
     git           # Git section (git_branch + git_status)
-    docker_compose
-    venv          # virtualenv section
-    conda         # conda virtualenv section
     async
     exec_time     # Execution time
+    nix_shell
     line_sep      # Line break
     exit_code     # Exit code section
     vi_mode
@@ -14,9 +12,15 @@ SPACESHIP_PROMPT_ORDER=(
 )
 
 SPACESHIP_RPROMPT_ORDER=(
+    ansible
+    docker
+    docker_compose
+    venv          # virtualenv section
+    conda         # conda virtualenv section
     node
     golang
     java
+    rust
     package
     time          # Time stamps section
 )
@@ -24,10 +28,25 @@ SPACESHIP_RPROMPT_ORDER=(
 # sections
 SPACESHIP_TIME_SHOW=true
 SPACESHIP_PACKAGE_SHOW=true
+SPACESHIP_VENV_SHOW=true
+SPACESHIP_CONDA_SHOW=true
 SPACESHIP_NODE_SHOW=true
 SPACESHIP_GOLANG_SHOW=true
 SPACESHIP_JAVA_SHOW=true
+SPACESHIP_RUST_SHOW=true
+SPACESHIP_RUST_VERBOSE_VERSION=true
+SPACESHIP_ANSIBLE_SHOW=true
+SPACESHIP_DOCKER_SHOW=true
 SPACESHIP_DOCKER_COMPOSE_SHOW=true
+
+# Icons
+SPACESHIP_JAVA_SYMBOL=
+SPACESHIP_GOLANG_SYMBOL=
+SPACESHIP_NODE_SYMBOL=󰎙
+SPACESHIP_DOCKER_COMPOSE_SYMBOL=
+SPACESHIP_DOCKER_SYMBOL=
+SPACESHIP_RUST_SYMBOL=
+
 
 # vi_mode
 spaceship_vi_mode_enable
