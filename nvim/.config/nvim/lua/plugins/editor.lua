96239164721fe0cdfb7af61c5630cf839d449901
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2024-02-23 14:44:56 +0530

    (nvim) add nvim ufo

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -9,6 +9,46 @@ return {
         end,
     },
     {
+        "kevinhwang91/nvim-ufo",
+        dependencies = {
+            "kevinhwang91/promise-async",
+        },
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            vim.o.foldcolumn = "1" -- '0' is not bad
+            vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+            require("ufo").setup({
+                open_fold_hl_timeout = 150,
+                close_fold_kinds = { "imports", "comment" },
+                preview = {
+                    win_config = {
+                        border = { "", "─", "", "", "", "─", "", "" },
+                        winhighlight = "Normal:Folded",
+                        winblend = 0,
+                    },
+                    mappings = {
+                        scrollU = "<C-u>",
+                        scrollD = "<C-d>",
+                        jumpTop = "[",
+                        jumpBot = "]",
+                    },
+                },
+            })
+            vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+            vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+            vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+            vim.keymap.set("n", "zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+            vim.keymap.set("n", "K", function()
+                local winid = require("ufo").peekFoldedLinesUnderCursor()
+                if not winid then
+                    vim.lsp.buf.hover()
+                end
+            end)
+        end,
+    },
+    {
         "nvim-treesitter/nvim-treesitter",
         version = false, -- last release is way too old and doesn't work on Windows
         build = ":TSUpdate",
