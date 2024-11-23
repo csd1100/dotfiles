ed4d37e5e960a9237725abc89223bd328c9b3a4b
Chaitanya Deshpande <cdeshpande@proton.me>
2024-11-24 00:11:31 +0530

    (nvim) add persistence

--- common/dot-config/nvim/lua/plugins/utils.lua
+++ common/dot-config/nvim/lua/plugins/utils.lua
@@ -5,4 +5,9 @@ return {
             {'<leader>u', vim.cmd.UndotreeToggle, desc = 'Toggle UndoTree'}
         },
     },
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre",
+        opts = { },
+    }
 }
