300a24b89b808ec5c759b799338db76d5f1e65cb
Chaitanya Deshpande <cdeshpande@proton.me>
2024-11-23 21:40:01 +0530

    (nvim) add lessspace.vim
    - this plugin removes trailing whitespaces only for edited lines

--- common/dot-config/nvim/lua/plugins/editor.lua
+++ common/dot-config/nvim/lua/plugins/editor.lua
@@ -1,6 +1,14 @@
 return {
     {
         'nkakouros-original/numbers.nvim',
+        event = 'InsertEnter',
         config = true,
+    },
+    {
+        'thirtythreeforty/lessspace.vim',
+        event = 'InsertEnter',
+        init = function()
+            vim.g.lessspace_blacklist = { 'markdown' }
+        end,
     },
 }
