873c18572f5f4ed14e3c6c46af59ee0403cddb56
Chaitanya Deshpande <cdeshpande@proton.me>
2025-01-01 19:42:06 +0530

    (nvim) add null-ls

--- common/dot-config/nvim/lua/plugins/lsp.lua
+++ common/dot-config/nvim/lua/plugins/lsp.lua
@@ -228,4 +228,19 @@ return {
       })
     end,
   },
+  {
+    'nvimtools/none-ls.nvim',
+    config = function()
+      local null_ls = require('null-ls')
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.completion.spell,
+          null_ls.builtins.formatting.shfmt,
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
+        },
+      })
+    end,
+  },
 }
