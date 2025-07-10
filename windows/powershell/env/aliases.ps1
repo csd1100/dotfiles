575f2b57ef8efe70aaa32512146d350289a771a7
Chaitanya Deshpande <cdeshpande@proton.me>
2025-07-10 19:35:13 +0530

    (windows): add aliases

--- windows/powershell/env/aliases.ps1
+++ windows/powershell/env/aliases.ps1
@@ -64,6 +64,17 @@ function dcst() {
     docker compose stop $args
 }
 
-function nv() {
-    nvim $args
+function which {
+    (Get-Command $args).Source
+}
+
+function finder {
+    param (
+        [string]$Pattern
+    )
+    $dir = fd -L --full-path --min-depth 1 -d 3 -t d ".*$Pattern.*" "$env:USERPROFILE/work" | fzf
+    if (-not [string]::IsNullOrWhiteSpace($dir)) {
+        echo "cd $dir"
+        cd $dir
+    }
 }
