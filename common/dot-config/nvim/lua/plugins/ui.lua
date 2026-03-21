4c7abf1afd72d47802746b60ff034ae734ec6d85
Chaitanya Deshpande <cdeshpande@proton.me>
2026-03-21 09:17:38 +0530

    (nvim) disable rainbow-delimiters due to incompatibility with nvim 12

--- common/dot-config/nvim/lua/plugins/ui.lua
+++ common/dot-config/nvim/lua/plugins/ui.lua
@@ -207,29 +207,29 @@ return {
       }
     end,
   },
-  {
-    url = 'https://gitlab.com/HiPhish/rainbow-delimiters.nvim.git',
-    event = 'VeryLazy',
-    init = function()
-      local rainbow_delimiters = require('rainbow-delimiters')
-
-      vim.g.rainbow_delimiters = {
-        strategy = {
-          [''] = rainbow_delimiters.strategy['global'],
-          vim = rainbow_delimiters.strategy['local'],
-        },
-        query = {
-          [''] = 'rainbow-delimiters',
-          lua = 'rainbow-blocks',
-        },
-        priority = {
-          [''] = 110,
-          lua = 210,
-        },
-        highlight = rainbow_highlight,
-      }
-    end,
-  },
+  -- {
+  --   url = 'https://gitlab.com/HiPhish/rainbow-delimiters.nvim.git',
+  --   event = 'VeryLazy',
+  --   init = function()
+  --     local rainbow_delimiters = require('rainbow-delimiters')
+  --
+  --     vim.g.rainbow_delimiters = {
+  --       strategy = {
+  --         [''] = rainbow_delimiters.strategy['global'],
+  --         vim = rainbow_delimiters.strategy['local'],
+  --       },
+  --       query = {
+  --         [''] = 'rainbow-delimiters',
+  --         lua = 'rainbow-blocks',
+  --       },
+  --       priority = {
+  --         [''] = 110,
+  --         lua = 210,
+  --       },
+  --       highlight = rainbow_highlight,
+  --     }
+  --   end,
+  -- },
   {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
